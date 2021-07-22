853633504e669b28be1a64bc05e1ba2523276ed5
zeuslearning-hiten-chawda <chawdahiten45@gmail.com>
2021-07-22 14:33:48 +0530

    Walk-in ER Diagram

--- 08 09-07-2021/quantum-screen3-task/index.js
+++ 08 09-07-2021/quantum-screen3-task/index.js
@@ -9,11 +9,11 @@ fetch("./data.json")
 
         `<div id="item${item.id}">
         <div class="box">
-            <img src="/quantum screen assets/quantum screen assets/images/${item.image}" alt="">
+            <img src="./quantum screen assets/quantum screen assets/images/${item.image}" alt="">
             <div class="con">
                 <div id="con-1">
                     <div id="title">${item.title}</div>
-                    <img src="/quantum screen assets/quantum screen assets/icons/${item.favourite ? 'favourite.svg' : 'favourite-unchecked.png'}" 
+                    <img src="./quantum screen assets/quantum screen assets/icons/${item.favourite ? 'favourite.svg' : 'favourite-unchecked.png'}" 
                     alt="">
                 </div>
                 <div id="con-2">
@@ -33,7 +33,7 @@ fetch("./data.json")
                     <span id="t" ${item.classes === "No Classes" ? `style="color: #6b6b6b;"` : ''}>
                         ${item.classes}
                     </span> 
-                    <img src="/quantum screen assets/quantum screen assets/icons/arrow-down.svg" alt="">
+                    <img src="./quantum screen assets/quantum screen assets/icons/arrow-down.svg" alt="">
                 </div>
                 <div id="con-5">
                 ${item.students ? `<span>${item.students} Students</span>` : ''}
@@ -46,16 +46,16 @@ fetch("./data.json")
         <hr style="clear: both;color:	#BEBEBE;margin: 20px 20px 20px 0 ;">
         <div class="box2">
             <div >
-                <img src="/quantum screen assets/quantum screen assets/icons/preview.svg" alt="">
+                <img src="./quantum screen assets/quantum screen assets/icons/preview.svg" alt="">
             </div>
             <div >
-                <img src="/quantum screen assets/quantum screen assets/icons/manage course.svg" alt="">
+                <img src="./quantum screen assets/quantum screen assets/icons/manage course.svg" alt="">
             </div>
             <div >
-                <img src="/quantum screen assets/quantum screen assets/icons/grade submissions.svg" alt="">
+                <img src="./quantum screen assets/quantum screen assets/icons/grade submissions.svg" alt="">
             </div>
             <div >
-                <img src="/quantum screen assets/quantum screen assets/icons/reports.svg" alt="">
+                <img src="./quantum screen assets/quantum screen assets/icons/reports.svg" alt="">
             </div>
         </div>
     </div>`
